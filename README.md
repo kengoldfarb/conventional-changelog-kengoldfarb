34b20fefd5a56afb9d27735370acf6ec394c107d
Ken <hello@kengoldfarb.com>
2020-07-15 16:33:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## A sensible conventional changelog preset
+
+### Credit
+
+Based on [conventional-changelog-sprucelabs](https://github.com/sprucelabsai/conventional-changelog-sprucelabs)
---
+++ conventional-recommended-bump.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const parserOpts = require(`./parser-opts`)
+
+module.exports = {
+	parserOpts,
+
+	whatBump: commits => {
+		// level 0 = major
+		// level 0 = minor
+		// level 2 = patch
+
+		let level = 2
+		let patch = 0
+		let minor = 0
+		let major = 0
+
+		commits.forEach(commit => {
+			switch (commit.type) {
+				case 'BREAKING':
+				case 'breaking':
+					major += 1
+					level = 0
+					break
+				case 'major':
+				case 'minor':
+				case 'feat':
+					minor += 1
+					if (level === 2) {
+						level = 1
+					}
+					break
+				default:
+					patch += 1
+					break
+			}
+		})
+
+		return {
+			level: level,
+			reason: `Major: ${major} | Minor: ${minor} | Patch: ${patch}`
+		}
+	}
+}
